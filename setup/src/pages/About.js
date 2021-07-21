23761ba7b4c5b080f88c37f2cbea0d893c8bf0d9
Ram <ramkrishnajha999@gmail.com>
2021-07-21 13:31:29 +0530

    Second commit

---
+++ setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        inventore et consequatur impedit vero doloribus ipsam? Voluptatum saepe
+        non vero et eos dolorem quos suscipit aperiam odit cupiditate, debitis
+        beatae possimus, quaerat ad vitae assumenda nobis quasi aspernatur aut
+        excepturi.
+      </p>
+    </section>
+  )
+}
+
+export default About
